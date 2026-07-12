e60c831977b50d0ad5990a44a636a3a6b4705dbf
tiozao <marco_aurelioo@yahoo.com.br>
2026-07-12 12:52:31 -0300

    primeiro commit.

---
+++ src/main/java/com/tiozao/PrValidationTest/PrValidationTestApplication.java
@@ -0,0 +1,13 @@
+package com.tiozao.PrValidationTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrValidationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrValidationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiozao/PrValidationTest/PrValidationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiozao.PrValidationTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrValidationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
