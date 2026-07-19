e90fd477c2c1c83e9b9668c6644801e4c4da7e86
tiozao <marco_aurelioo@yahoo.com.br>
2026-07-18 23:28:15 -0300

    teste de pr.

---
+++ src/main/java/com/tiozao/PrValidationTest/service/ContaService.java
@@ -0,0 +1,11 @@
+package com.tiozao.PrValidationTest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+
+    public void createConta(){
+        System.out.println("Criando Conta");
+    }
+}
