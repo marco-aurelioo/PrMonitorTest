55ddeddfbe9098c88524c771b56fed31cc4cbead
tiozao <marco_aurelioo@yahoo.com.br>
2026-07-19 23:33:05 -0300

    novos metodos.

--- src/main/java/com/tiozao/PrValidationTest/service/ContaService.java
+++ src/main/java/com/tiozao/PrValidationTest/service/ContaService.java
@@ -2,10 +2,21 @@ package com.tiozao.PrValidationTest.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ContaService {
 
     public void createConta(){
         System.out.println("Criando Conta");
+    }
+
+    public void deleteConta(){
+        System.out.println("Deletando Conta");
+    }
+
+    public List<String> getListagem(){
+        return new ArrayList<>();
     }
 }
